64b401f2948ebe806b895a81e8c670d2fc5f7060
moritzgauss <moritzgg99@gmail.com>
2024-09-30 20:31:24 +0200

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    const loader = document.getElementById('loader');
+    const loadingText = document.getElementById('loadingText');
+    
+    let typingEffect = setInterval(() => {
+        if (loadingText.textContent.length < 10) {
+            loadingText.textContent += ".";
+        } else {
+            loadingText.textContent = "Loading";
+        }
+    }, 300);
+    
+    // Fading out loader after 2 seconds
+    setTimeout(() => {
+        clearInterval(typingEffect);
+        loader.classList.add('hidden');
+    }, 2000);
+};
