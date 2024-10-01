c2e383834a59a23ab765e0139fce8b866e4799c6
moritzgauss <moritzgg99@gmail.com>
2024-10-01 02:08:21 +0200

    backup

--- script1.js
+++ script1.js
@@ -13,4 +13,23 @@ window.onload = function() {
     // Fading out loader after 2 seconds
     setTimeout(() => {
         clearInterval(typingEffect);
-        loader.classList.add('hidden'); // Hide loader after 2 se
+        loader.classList.add('hidden'); // Hide loader after 2 seconds
+    }, 2000);
+};
+
+function openFolder(event) {
+    event.stopPropagation(); // Prevent triggering the document click
+    document.getElementById('folderOverlay').style.display = 'flex'; // Show folder overlay
+    document.querySelector('.blur-container').classList.add('active'); // Add blur class
+}
+
+function closeFolder() {
+    document.getElementById('folderOverlay').style.display = 'none'; // Hide folder overlay
+    document.querySelector('.blur-container').classList.remove('active'); // Remove blur class
+}
+
+// Close folder when clicking outside of it
+document.getElementById('folderOverlay').addEventListener('click', closeFolder);
+document.querySelector('.folder-content').addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent closing the folder when clicking inside
+});
